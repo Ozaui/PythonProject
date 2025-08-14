ecf7d1af962a33fa6bce8dcbbb69fcbd9088264f
Özay Melih Yıldız <102221334+Ozaui@users.noreply.github.com>
2025-08-14 22:03:40 +0300

    today's push

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+from random import randint
+from newModule import printName
+from AnimalPackage import info
+from AnimalPackage.CatPackage import meov
+
+info.info()
+meov.talk()
+
+
+example = np.zeros((2,3))
+print(example)
